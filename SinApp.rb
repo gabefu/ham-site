98a11ce4d6ce268aaf7e09c427a0c2d205474cb0
Mike Michaels <gabefu@gabefu.com>
2012-07-23 21:35:21 -0500

    Sinatra test with multi-line string

---
+++ SinApp.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sinatra'
+get '/hi' do
+	"<html>
+		<head> </head>
+		<body>
+			<p>
+				I Did It My Way!
+			</p>
+			<p>
+			But Why?
+			</p>
+		</body>
+	</html>"
+end
