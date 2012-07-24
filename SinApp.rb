e5213105aa3ea07446ab6b59bb7f1e31c3576d33
Mike Michaels <gabefu@gabefu.com>
2012-07-23 22:24:28 -0500

    Simple 'post' and 'get' example

--- SinApp.rb
+++ SinApp.rb
@@ -1,15 +1,36 @@
 require 'rubygems'
 require 'sinatra'
+
+i = 0
+msg = "No one has left a message yet."
+
+# HTTP 'GET' request
 get '/hi' do
+	i+=1
 	"<html>
 		<head> </head>
 		<body>
+			<script>
+				
+			</script>
 			<p>
-				I Did It My Way!
+				I Did It My Way! #{i} times!
 			</p>
 			<p>
-			But Why?
+				Message from previous page view:
 			</p>
+			<h1>#{msg}</h1>
+			<p>Want to respond? Do so below:</p>
+			
+			<form action='/thing' method='post'>
+				<input id='bob' name='msg'></input>
+			</form>
 		</body>
 	</html>"
+end
+# HTTP 'POST' (forms)
+post '/thing' do
+	msg = params[:msg]
+	p "They told us #{msg}"
+	redirect to('/hi')
 end
